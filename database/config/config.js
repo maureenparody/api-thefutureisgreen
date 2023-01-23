e84708fc97bec29bcf152cabae35a52723f1c788
maureenparody <maureenparody@gmail.com>
2023-01-23 16:51:20 -0300

    los modelos

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,37 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST, //revisar si esta bien conectado a mamp
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT,
+    "define": {
+      "timeStamp": false,
+      "createdAt": false,
+      "freezeTableName": true,
+      "camelCase": false,
+      paranoid: true
+
+    }
+  }
+  ,
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST, //revisar si esta bien conectado a mamp
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST, //revisar si esta bien conectado a mamp
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT
   }
 }
